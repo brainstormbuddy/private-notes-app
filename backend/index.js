319cefe38e8a73cbd1b1b248c2c3b07b945e66ee
Adam Sims <adamsims1203@gmail.com>
2021-01-26 10:40:26 +0100

    Add basic user creation route

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,11 @@ const mongoose = require('mongoose');
 
 const usersRoute = require('./routers/users');
 
-// API routes
-app.use('/api/users', usersRoute);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// API routes
+app.use('/api/users', usersRoute);
 
 // Connect to mongodb
 mongoose.connect('mongodb://localhost/notes-db', { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('Connected to mongodb'));
--- backend/routers/users.js
+++ backend/routers/users.js
@@ -3,8 +3,29 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const userModel = require('../models/user.model');
 
-router.get('/', (req, res) => {
-  res.send('Hello!');
+router.get('/', async (req, res) => {
+  try {
+    const users = await userModel.find();
+    res.json({ users });
+  }
+  catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.post('/', async (req, res) => {
+  const user = new userModel({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  try {
+    const savedUser = await user.save();
+    res.json({ savedUser });
+  }
+  catch (err) {
+    res.json({ message: err });
+  }
 });
 
 module.exports = router;
