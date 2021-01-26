d8cc34bd5340e5d9aac3a3e4d56e78f040edb770
Adam Sims <adamsims1203@gmail.com>
2021-01-26 10:07:07 +0100

    Add connection to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
 
 const usersRoute = require('./routers/users');
 
@@ -9,6 +10,9 @@ app.use('/api/users', usersRoute);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Connect to mongodb
+mongoose.connect('mongodb://localhost/notes-db', { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('Connected to mongodb'));
 
 app.listen(port, () => {
   console.log(`Listening at port: ${port}`);
