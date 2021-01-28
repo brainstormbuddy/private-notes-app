1ff9d5d2f84c3262b1e006f3738801bb31fa541f
Adam Sims <adamsims1203@gmail.com>
2021-01-28 22:42:04 +0100

    Add password encryption with salt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -6,4 +6,32 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true, minLength: 1, maxLength: 20 },
 }, { timestamps: true });
 
+userSchema.pre('save', function(next) {
+  var user = this;
+
+  // Only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // Generate a salt
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) return next(err);
+
+    // Hash the password using our new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      // Override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
 module.exports = mongoose.model('User', userSchema);
--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -32,18 +32,22 @@ router.post('/login', async (req, res) => {
   try {
     // Check if user exists
     const user = await userModel.findOne({ username: req.body.username }).exec();
-    if(user === null) return res.status(400).json({ message: 'User does not exists!' });
+    if(user === null) return res.status(400).json({ message: 'Bad login or password!' });
 
-    // Generate jwt bearer token 
-    const jwtBearerToken = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
-      algorithm: 'HS256',
-      expiresIn: process.env.JWT_EXPIRES_IN,
-    });
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) return res.status(400).json({ message: 'Bad login or password!' });
 
-    res.status(200).json({
-      username: req.body.username,
-      idToken: jwtBearerToken,
-      expiresIn: process.env.JWT_EXPIRES_IN
+      // Generate jwt bearer token 
+      const jwtBearerToken = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        algorithm: 'HS256',
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      });
+
+      res.status(200).json({
+        username: req.body.username,
+        idToken: jwtBearerToken,
+        expiresIn: process.env.JWT_EXPIRES_IN
+      });
     });
   }
   catch (err) {
